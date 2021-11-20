7ff816de1ed7dcb68438c8ad4cb5e0c588787e3f
Pranta07 <palpranta@gmail.com>
2021-11-20 15:50:36 +0600

    dynamic route setup using getStaticPaths

---
+++ pages/users/[id].js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const User = ({ user }) => {
+    return (
+        <div>
+            <h2>Single User id: {user.id}</h2>
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+        </div>
+    );
+};
+
+// This function gets called at build time
+export async function getStaticPaths() {
+    // Call an external API endpoint to get posts
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+
+    // Get the paths we want to pre-render based on posts
+    const paths = users.map((user) => ({
+        params: { id: user.id.toString() },
+    }));
+
+    // We'll pre-render only these paths at build time.
+    // { fallback: false } means other routes should 404.
+    return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+    // params contains the post `id`.
+    // If the route is like /posts/1, then params.id is 1
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${params.id}`
+    );
+    const user = await res.json();
+
+    // Pass post data to the page via props
+    return { props: { user } };
+}
+
+export default User;
